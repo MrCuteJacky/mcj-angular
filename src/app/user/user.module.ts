9ef563e86aba6aee0eefd78851a5e04c0f26d24c
Jacky <mrcutejacky@gmail.com>
2020-12-02 11:11:05 +0800

    code style.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,21 +3,19 @@ import {CommonModule} from '@angular/common';
 import {UserFormComponent} from './user-form/user-form.component';
 import {UserManagerComponent} from './user-manager/user-manager.component';
 import {FormsModule} from '@angular/forms';
-import {RouterModule, Routes} from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: UserManagerComponent
-  }
-];
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [UserFormComponent, UserManagerComponent],
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild([
+      {
+        path: '',
+        component: UserManagerComponent
+      }
+    ])
   ]
 })
 export class UserModule { }
